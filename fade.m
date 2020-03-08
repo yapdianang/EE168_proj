af91d38c13d548b3853cfebe4daf9cd4ac47e54e
Dian Ang Yap <dianang7@rice11.stanford.edu>
2020-03-07 17:50:08 -0800

    faded

--- fade.m
+++ fade.m
@@ -1,9 +1,11 @@
 function res = fade(im1, im2, time)
     % assumes that im1 and im2 are of the same size, time in seconds
+    im1 = imresize(im1, [512 512]);
+    im2 = imresize(im2, [512, 512]);
     numFrames = time * 25;
     for k = 1:numFrames
 
-        im_curr = im1(:, :, :) * k/numFranes + im2(:, :, :) * (1-k)/numFrames;
+        im_curr = im1(:, :, :) * k/numFrames + im2(:, :, :) * (1-k)/numFrames;
 
         imagesc(im_curr);
         M(k) = getframe(gcf);
